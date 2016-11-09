79bea6ee1d259e7f8d9e57693d260a9423d24b03
cassi fromer <cassi fromer>
2016-11-09 22:32:24 +0200

    homework class 1

---
+++ assignments/class1/home3.js
@@ -0,0 +1,15 @@
+function changeFont(){
+
+    if (currentFont == "inherit"){
+        document.body.style.fontFamily = "cursive";
+        currentFont = "cursive";
+    } else {
+        document.body.style.fontFamily = "fantasy";
+        currentFont = "fantasy";
+    }
+    return currentFont;
+
+}
+
+document.getElementById("myButton").onclick = changeFont;
+var currentFont = "inherit";
