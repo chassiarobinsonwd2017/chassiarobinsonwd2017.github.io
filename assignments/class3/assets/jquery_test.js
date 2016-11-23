9df966e26447786abf0883f51c648cc88aa223c0
cassi fromer <cassi fromer>
2016-11-23 10:36:49 +0200

    homework

---
+++ assignments/class3/assets/jquery_test.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    var myFooter = $(".footerDiv");
+
+
+ myFooter.on("mouseenter", function(){
+     $(this).fadeIn();
+ }).on("mouseleave", function(){
+     $(this).fadeOut();
+
+ });
+});
+
