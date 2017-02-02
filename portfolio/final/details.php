944c4268945e0f3453d095c0fcce5b4e103650e5
chassiarobinsonwd2017 <chazfromer@gmail.com>
2017-02-02 12:29:34 +0200

    gfjh

---
+++ portfolio/final/details.php
@@ -0,0 +1,40 @@
+
+<? include ("header.html");?>
+    <div class="container-fluid bg-3 text-center">
+
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="86cannon_0004_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="thelondonloom_0007_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="oneandother.co2_0010_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+
+  </div>
+</div>
+
+<div class="container-fluid bg-3 text-center">
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="candccoffee_0006_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="vioski_0008_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4">
+      <p>Some text..</p>
+      <img src="idahoffart.dk_0005_1.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+
+  </div>
+</div>
+<? include ("footer.html");?>
+
---
+++ portfolio/final/header.php
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?=$page_title?></title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="main.css" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    </head>
+<body>
+
+    <div class="container-fluid">
+        <div class="row nav">
+
+
+          <div class="col-sm-3"> <li><img src="new.jpg"></li></div>
+               <div class="col-sm-6 col-sm-offset-3">
+                   <ul>
+                     <li>Work</li>
+                    <li>About</li>
+                    <li>Contact</li>
+
+                </ul>
+
+            </div>
+
+
+    </div>
+    </div>
